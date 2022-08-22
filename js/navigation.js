97936896f209e3e8ee83d52646a7787f91e82893
dylanconklin <74876741+dylanconklin@users.noreply.github.com>
2022-08-22 02:55:57 -0700

    Add files via upload

--- js/navigation.js
+++ js/navigation.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
-    $("#content").load("../html/about.html");
+    let hash = getpage(window.location.hash);
+    loadpage(hash);
     $("#about").click(function(){
         $("#content").load("../html/about.html");
     });
     $("#previous").click(function(){
         $("#content").load("../html/previous.html");
     });
+    $("#table").click(function(){
+        $("#content").load("../html/table.html");
+    });
     $("#projects").click(function(){
         $("#content").load("../html/projects.html");
     });
-    $("#contact").click(function(){
-        $("#content").load("../html/contact.html");
-    });
 });
+
+function loadpage(hash){
+    let htmlpath = "../html/";
+    let content = htmlpath + hash + ".html";
+    $("#content").load(content);
+}
+
+function getpage(hash){
+    if(hash.length == 0){
+        hash = "about";
+    }else{
+        hash = hash.substring(1, hash.length);
+    }
+    return hash;
+}
